d7b4365a53b66e5ce3f98150455bdea545991ad9
Rajasekar Elango <e.rajasekar@gmail.com>
2012-07-26 00:19:42 -0400

    Cleaned up duplicate items

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -52,37 +52,12 @@ class BootStrap {
 
         //1-5
         itemService.createItem('exercise','perimeter1.xml');
-        itemService.createItem('qti','choice_fixed.xml');
-        itemService.createItem('qti','choice_multiple.xml');
         itemService.createItem('qti','choice_multiple_chocolate.xml');
-        itemService.createItem('qti','inline_choice.xml');
-
-        //6-10
         itemService.createItem('qti','feedback.xml');
-        itemService.createItem('qti','hint.xml');
         itemService.createItem('qti','adaptive.xml');
-        itemService.createItem('qti','adaptive_template.xml');
-        itemService.createItem('qti','template.xml');
-
-        //11-15
-        itemService.createItem('qti','template_image.xml');
-        itemService.createItem('qti','math.xml');
-        itemService.createItem('qti','MAB01a.xml');
-        itemService.createItem('qti','MA-UT-01a-NoTP-Choice.xml');
-        itemService.createItem('qti','MA-UT-01c-NoTP-Num.xml');
-
-        //16-20
-        itemService.createItem('exercise/Std10','Sequences_Ex_2_1.xml');
-        itemService.createItem('qti','likert.xml');
         itemService.createItem('qti','mathml-templated.xml');
         itemService.createItem('qti','content-with-html-tags.xml');
-        itemService.createItem('qti','order.xml');
 
-        //21-25
-        itemService.createItem('qti','match.xml');
-        itemService.createItem('qti','associate.xml');
-        itemService.createItem('qti','hottext.xml');
-        itemService.createItem('qti','gap_match.xml');
     }
 
     def createTests() {
