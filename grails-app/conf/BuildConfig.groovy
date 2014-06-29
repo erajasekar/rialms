a825e95232b1c2565ccb16d38fa2c1eb9c29ebdd
Rajasekar Elango <e.rajasekar@gmail.com>
2014-06-29 17:40:57 -0400

    Still working on resource plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,6 +9,21 @@ grails.project.source.level = 1.6
 
 verboseCompile = true
 
+grails.project.fork = [
+        // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+        //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+        // configure settings for the test-app JVM, uses the daemon by default
+        test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+        // configure settings for the run-app JVM
+        run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the run-war JVM
+        war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the Console UI JVM
+        console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -45,7 +60,7 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":hibernate4:4.3.5.4"
         runtime ":jquery:1.11.1"
-        compile ":jquery-ui:1.8.15"
+        compile ":jquery-ui:1.10.3"
         runtime ":resources:1.2.8"
       //  compile ":coffeescript-resources:0.2"
         compile ":coffeescript-resources:0.3.2"
